0fd537af4fd2c793ab6e5d07923f3900458c7eb3
mariano alvarez <malvarezh92@gmail.com>
2016-10-02 19:49:21 -0600

    init imgcache library

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { AboutPage }   from './about/about';
+export { ContactPage } from './contact/contact';
+export { HomePage }    from './home/home';
+export { TabsPage }    from './tabs/tabs';
